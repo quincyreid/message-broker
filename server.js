191f783093dbd1b94c9a0100fdf3ce8e9857a7f4
Quincy Reid <qreid51@gmail.com>
2016-11-21 20:53:37 -0500

    Adds body parser

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 /**
  * Server
  */
-
+let bodyParser = require('body-parser')
 let express = require('express')
 let app = express()
+
+app.use(bodyParser.json())
 
 if (!module.parent) {
   app.listen(process.env.PORT || 3030)
