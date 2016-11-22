321f112adf6a046998f495488b2d618f15c5ac60
Quincy Reid <qreid51@gmail.com>
2016-11-22 06:38:52 -0500

    Adds amqplib and sendgrid

--- server.js
+++ server.js
@@ -3,9 +3,13 @@
  */
 const bodyParser = require('body-parser')
 const express = require('express')
+
 const app = express()
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+const messageBroker = require('./lib/message-broker')
+app.use('/', messageBroker)
 
 if (!module.parent) {
   app.listen(process.env.PORT || 3030)
